7d2dfa5771272c8a2dbb7a0746231fa3604cd845
Filipo Negrao <filiposilver@hotmail.com>
2017-12-11 01:08:21 -0200

    ajustes

--- middleware/set_db.go
+++ middleware/set_db.go
@@ -9,7 +9,6 @@ func SetDBtoContext(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, origin")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 		c.Set("DB", db)
