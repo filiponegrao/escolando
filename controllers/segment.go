3f0a0fc484f71ea55c576274dabbb2855804f3d7
filiponegrao <filiposilver@hotmail.com>
2018-09-23 18:15:40 -0300

    versao estabilizada

---
+++ controllers/segment.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetSegments(c *gin.Context) {
+
+}
+
+func GetInsitutionSegments(c *gin.Context) {
+
+}
+
+func GetSegment(c *gin.Context) {
+
+}
+
+func CreateSegment(c *gin.Context) {
+
+}
+
+func UpdateSegment(c *gin.Context) {
+
+}
+
+func DeleteSegment(c *gin.Context) {
+
+}
