02ca1a7fcb00678c9d04c942bcb0da7ed081ac62
Filipo Negrao <filiposilver@hotmail.com>
2018-12-10 23:44:33 -0200

    ajustes em requisicao de parente por usuario, mensagens por aluno e outros

--- db/db.go
+++ db/db.go
@@ -3,23 +3,26 @@ package db
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/filiponegrao/escolando/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
 	"github.com/serenize/snaker"
 )
 
 func Connect() *gorm.DB {
 	// SQLITE
-	// dir := filepath.Dir("db/database.db")
-	// db, err := gorm.Open("sqlite3", dir+"/database.db")
+	dir := filepath.Dir("db/database.db")
+	db, err := gorm.Open("sqlite3", dir+"/database.db")
 
 	// POSTGRES
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=escolando password=postgres")
+	// db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=escolando password=postgres")
 	if err != nil {
 		log.Fatalf("Got error when connect database, the error is '%v'", err)
 	}
